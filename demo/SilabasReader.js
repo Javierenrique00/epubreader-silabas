57364a5e41d03fc2a570face8c17ad7543e9b93a
javierenrique00@hotmail.com <javierenrique00@hotmail.com>
2018-09-24 21:42:16 -0500

    working 1.1

--- demo/SilabasReader.js
+++ demo/SilabasReader.js
@@ -8,17 +8,28 @@ const Div = styled.div`
     color: black;
 `
 
+
+function Linea(props){
+    return (<p>{props.linea}</p>)
+}
+
+
 class SilabasReader extends Component{
     constructor(props){
         super(props)
         this.lineArray = []
+        this.lineCursor = 0;
+        this.wordArray = [];
+        this.wordCursor = 0;
         this.state = {
-            miTexto: "",
+            linea: "",
+            palabra: "",
         }   
     }
 
 
     componentWillReceiveProps(nextProps) {
+        //--- filtra la entrada para dejar un vector de lineas de texto
         let re = /\n/;
         let re2 = /^\s*/;
         let tempArray = nextProps.sendText.split(re);
@@ -26,22 +37,77 @@ class SilabasReader extends Component{
             let salida=elemento.replace(re2, '')
             if(salida!="") this.lineArray.push(salida)
         })
-       
-        this.setState({miTexto:this.lineArray[0]})
+        let re3 = /\s/
+        this.wordArray = this.lineArray[this.lineCursor].split(re3)
     }
     
-    componentWillMount() {
+    showLine(){
+        //-- hace la conversión a vector de palabras
+        let re = /\s/
+        this.wordArray = this.lineArray[this.lineCursor].split(re)
+        this.setState({linea:this.lineArray[this.lineCursor]})
+        this.setState({palabra:this.wordArray[this.wordCursor]})
+    }
 
+
+
+    //------ Tecla de avanzar
+    nextKey(){
+        //--mira si puede avanzar
+        console.log("NEXT wcursor=",this.wordCursor)
+        //--- avanza la palabra
+        let wtam = this.wordArray.length
+        if(this.wordCursor+1<wtam) {
+            this.wordCursor++
+        }else{
+            //--- avanza la linea
+            let tama = this.lineArray.length
+            if(this.lineCursor+1<tama) this.lineCursor++
+            this.wordCursor = 0;
+        }
+        //--muestra la linea
+        this.showLine();
+
+    }
+
+    //------ Tecla de retroceder
+    prevKey(){
+        
+    }
+
+
+    _handleKeyDown = (event) => {
+        //console.log("Tecla="+event.keyCode); //---88(x) y 90(z)
+        switch(event.keyCode){
+            case 88:
+                this.nextKey();
+            break
+            
+            case 90:
+                this.prevKey();
+            break
+            default:
+            break
+
+        }
+    }
+
+
+    componentWillMount() {
+        document.addEventListener("keydown", this._handleKeyDown.bind(this));
+        //this.showLine();
     }
 
     componentWillUnmount() {
-    
+        document.removeEventListener("keydown", this._handleKeyDown.bind(this));
     }
 
     render(){
         return (
         <Div>
-            {this.state.miTexto}
+            <Linea linea={this.state.linea}/>
+            <br/>
+            <Linea linea={this.state.palabra}/>
         </Div>
         )}
 }
